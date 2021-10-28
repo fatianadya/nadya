f2ded0d3990b1f2155bce67b8e74b96a82f29f0a
fatianadya <89918362+fatianadya@users.noreply.github.com>
2021-10-28 18:54:24 +0700

    Add files via upload

---
+++ js/main-detail.js
@@ -0,0 +1,34 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit',function(event) {
+  event.preventDefault();
+  const newTodo = document.querySelector('input[name=newTodo]').value;
+  addTodo(newTodo);
+});
+
+
+
+function addTodo(newTodo) {
+  //membuat element li
+  const li = document.createElement('li');
+  li.textContent = newTodo;
+  console.log(li);
+  //membuat element button hapus
+  const deleteButton = document.createElement('button');
+  deleteButton.textContent = 'X';
+  deleteButton.addEventListener('click',function(){
+    deleteTodo(this);
+  });
+  
+  li.appendChild(deleteButton);
+  //append ke ul
+  const ul = document.querySelector('ul');
+  ul.appendChild(li);
+  
+  // console.log(ul);
+}
+
+function deleteTodo(todo) {
+  const li = todo.parentNode
+  li.remove();
+}
